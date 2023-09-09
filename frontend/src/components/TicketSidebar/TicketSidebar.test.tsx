5b726b6ac1ca9c429905be2bc2a5e84d27067653
“Sandra <sandravargasgro@gmail.com>
2023-09-08 21:36:56 -0700

    Fix TicketSidebar test

--- frontend/src/components/TicketSidebar/TicketSidebar.test.tsx
+++ frontend/src/components/TicketSidebar/TicketSidebar.test.tsx
@@ -6,13 +6,14 @@ import {
   fakePopulatedTickets,
   fakeStaffUser,
 } from "../../__mocks__";
+import { getStatusText } from "../../utils";
 
 const fakeTicket = fakePopulatedTickets[0];
 
 describe("TicketSidebar", () => {
   test("renders correctly", () => {
     render(<TicketSidebar ticket={fakeTicket} />);
-    const element = screen.getByText(fakeTicket.status);
+    const element = screen.getByText(getStatusText(fakeTicket.status));
     expect(element).toBeInTheDocument();
   });
 
