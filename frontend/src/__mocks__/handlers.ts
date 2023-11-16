3289c0816dc93d37a823c2a808e3db6865f2fec5
“Sandra <sandravargasgro@gmail.com>
2023-11-16 00:00:59 -0800

    Add post routes to handlers

--- frontend/src/__mocks__/handlers.ts
+++ frontend/src/__mocks__/handlers.ts
@@ -23,6 +23,10 @@ export const handlers: HttpHandler[] = [
     const fakeUser = fakeUsers.find((user) => user._id === params.id);
     return HttpResponse.json(fakeUser, { status: 200 });
   }),
+  http.post(`${baseUrl}${USERS_BASE_API_URL}`, async ({ request }) => {
+    const newUser = await request.json();
+    return HttpResponse.json(newUser, { status: 201 });
+  }),
   http.get(`${baseUrl}${TICKETS_BASE_API_URL}`, () => {
     return HttpResponse.json(fakePopulatedTickets, { status: 200 });
   }),
@@ -32,11 +36,19 @@ export const handlers: HttpHandler[] = [
     );
     return HttpResponse.json(fakeTicket, { status: 200 });
   }),
+  http.post(`${baseUrl}${TICKETS_BASE_API_URL}`, async ({ request }) => {
+    const newTicket = await request.json();
+    return HttpResponse.json(newTicket, { status: 201 });
+  }),
   http.get(`${baseUrl}${COMPANIES_BASE_API_URL}`, () => {
     return HttpResponse.text(`Requested companies`, { status: 200 });
   }),
   http.get(`${baseUrl}${COMPANIES_BASE_API_URL}/:id`, ({ params }) => {
     return HttpResponse.text(`Company ${params.id}`, { status: 200 });
+  }),
+  http.post(`${baseUrl}${COMPANIES_BASE_API_URL}`, async ({ request }) => {
+    const newCompany = await request.json();
+    return HttpResponse.json(newCompany, { status: 201 });
   }),
   http.get(`${baseUrl}${SERVICES_BASE_API_URL}`, () => {
     return HttpResponse.json(fakeServices, { status: 200 });
@@ -44,16 +56,28 @@ export const handlers: HttpHandler[] = [
   http.get(`${baseUrl}${SERVICES_BASE_API_URL}/:id`, ({ params }) => {
     return HttpResponse.text(`Service ${params.id}`, { status: 200 });
   }),
+  http.post(`${baseUrl}${SERVICES_BASE_API_URL}`, async ({ request }) => {
+    const newService = await request.json();
+    return HttpResponse.json(newService, { status: 201 });
+  }),
   http.get(`${baseUrl}${COMMENTS_BASE_API_URL}`, () => {
     return HttpResponse.text(`Requested comments`, { status: 200 });
   }),
   http.get(`${baseUrl}${COMMENTS_BASE_API_URL}/:id`, ({ params }) => {
     return HttpResponse.text(`Comment ${params.id}`, { status: 200 });
   }),
+  http.post(`${baseUrl}${COMMENTS_BASE_API_URL}`, async ({ request }) => {
+    const newComment = await request.json();
+    return HttpResponse.json(newComment, { status: 201 });
+  }),
   http.get(`${baseUrl}${PROJECTS_BASE_API_URL}`, () => {
     return HttpResponse.json(fakeProjects, { status: 200 });
   }),
   http.get(`${baseUrl}${PROJECTS_BASE_API_URL}/:id`, ({ params }) => {
     return HttpResponse.text(`Project ${params.id}`, { status: 200 });
+  }),
+  http.post(`${baseUrl}${PROJECTS_BASE_API_URL}`, async ({ request }) => {
+    const newProject = await request.json();
+    return HttpResponse.json(newProject, { status: 201 });
   }),
 ];
