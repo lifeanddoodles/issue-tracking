d752e20d2e48105e8dc71d31efdbe1456e840569
“Sandra <sandravargasgro@gmail.com>
2023-11-16 00:02:49 -0800

    Update user routes' test

--- frontend/src/routes/routes.test.ts
+++ frontend/src/routes/routes.test.ts
@@ -1,19 +1,30 @@
-import { USERS_BASE_API_URL } from ".";
-import { baseUrl, fakeUsers } from "../__mocks__";
+import { USERS_BASE_API_URL, getPostOptions } from ".";
+import { baseUrl, fakeUsers, newFakeUser } from "../__mocks__";
 
 const fakeUser = fakeUsers[0];
 
-test("fetches users", async () => {
-  const response = await fetch(`${baseUrl}${USERS_BASE_API_URL}`);
+describe("User routes", () => {
+  test("fetches users", async () => {
+    const response = await fetch(`${baseUrl}${USERS_BASE_API_URL}`);
 
-  expect(response.status).toBe(200);
-  expect(await response.json()).toEqual(fakeUsers);
-});
+    expect(response.status).toBe(200);
+    expect(await response.json()).toEqual(fakeUsers);
+  });
 
-test("fetches the user info", async () => {
-  const userId = fakeUser._id;
-  const response = await fetch(`${baseUrl}${USERS_BASE_API_URL}/${userId}`);
+  test("given an ID, fetches user info", async () => {
+    const userId = fakeUser._id;
+    const response = await fetch(`${baseUrl}${USERS_BASE_API_URL}/${userId}`);
 
-  expect(response.status).toBe(200);
-  expect(await response.json()).toEqual(fakeUser);
+    expect(response.status).toBe(200);
+    expect(await response.json()).toEqual(fakeUser);
+  });
+
+  test("adds new user", async () => {
+    const options = getPostOptions(newFakeUser);
+    const response = await fetch(`${baseUrl}${USERS_BASE_API_URL}`, options);
+    const json = await response.json();
+
+    expect(response.status).toBe(201);
+    expect(json).toEqual(newFakeUser);
+  });
 });
