274b01098b1f40cc8db755053524fff3c69a00a3
“Sandra <sandravargasgro@gmail.com>
2024-02-10 02:46:09 -0800

    Refactor PageDetails, create its unit tests

---
+++ frontend/src/pages/resources/projects/ProjectDetails/ProjectDetails.test.tsx
@@ -0,0 +1,281 @@
+import { act, screen, waitFor } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { IProjectDocument, IUserDocument } from "shared/interfaces";
+import { vi } from "vitest";
+import ProjectDetails from ".";
+import { fakeClientUserNoCompany, fakeProjects } from "../../../../__mocks__";
+import {
+  getUseResourceInfoMockReturnValue,
+  requestUpdateResourceMock,
+  useResourceInfoMockReturnWithError,
+  useResourceInfoMockReturnWithSuccess,
+} from "../../../../__mocks__/useResourceInfo";
+import useResourceInfo from "../../../../hooks/useResourceInfo";
+import { FormElement, ResourceState } from "../../../../interfaces";
+import { PROJECTS_BASE_API_URL } from "../../../../routes";
+import {
+  IAuthContext,
+  adminAuth,
+  authBase,
+  compareValue,
+  componentWithAuthContext,
+  getFieldByLabel,
+  renderWithRouter,
+  renderWithRouterFromRoute,
+  testEach,
+  updateFieldAndCheckValue,
+} from "../../../../tests/utils";
+import { getReadableInputName } from "../../../../utils";
+
+vi.mock("../../../../hooks/useResourceInfo");
+
+const fieldsToTest = [
+  { fieldId: "name", newFieldValue: "New name" },
+  { fieldId: "url", newFieldValue: "https://new-url.com" },
+  { fieldId: "description", newFieldValue: "New description" },
+  { fieldId: "newService", newFieldValue: "1" },
+  { fieldId: "newTeamMember", newFieldValue: "1" },
+];
+
+const fieldsToTestNoSelects = [
+  { fieldId: "name", newFieldValue: "New name" },
+  { fieldId: "url", newFieldValue: "https://new-url.com" },
+  { fieldId: "description", newFieldValue: "New description" },
+];
+
+describe("ProjectDetails", () => {
+  let auth: IAuthContext;
+  let useResourceInfoMockReturn: ResourceState<unknown>;
+
+  describe("if project does not exist", async () => {
+    beforeEach(async () => {
+      useResourceInfoMockReturn = useResourceInfoMockReturnWithError;
+
+      await getUseResourceInfoMockReturnValue(
+        useResourceInfo,
+        useResourceInfoMockReturn
+      );
+
+      auth = {
+        user: {
+          ...fakeClientUserNoCompany,
+        } as IUserDocument,
+        ...authBase,
+      };
+    });
+
+    test("renders error", async () => {
+      act(() => renderWithRouter(<ProjectDetails />, auth));
+
+      const title = await screen.findByText(/error/i);
+      const notApplicableTitle = screen.queryByText(/project details/i);
+
+      expect(title).toBeInTheDocument();
+      expect(notApplicableTitle).not.toBeInTheDocument();
+    });
+  });
+
+  describe("if project exists", async () => {
+    let project: IProjectDocument;
+
+    beforeAll(() => {
+      vi.clearAllMocks();
+    });
+
+    beforeEach(async () => {
+      auth = {
+        ...adminAuth,
+      };
+
+      project = fakeProjects.find(
+        (project) => project.company === auth.user.company
+      ) as unknown as IProjectDocument;
+
+      useResourceInfoMockReturn = useResourceInfoMockReturnWithSuccess(project);
+
+      await getUseResourceInfoMockReturnValue(
+        useResourceInfo,
+        useResourceInfoMockReturn
+      );
+    });
+
+    test.each`
+      fieldId            | findBy
+      ${"heading"}       | ${"role"}
+      ${"name"}          | ${"label"}
+      ${"url"}           | ${"label"}
+      ${"description"}   | ${"label"}
+      ${"newService"}    | ${"label"}
+      ${"newTeamMember"} | ${"label"}
+    `(
+      "renders $fieldId with the correct value",
+      async ({ fieldId, findBy }) => {
+        act(() => renderWithRouter(<ProjectDetails />, auth));
+
+        await waitFor(async () => {
+          if (findBy === "role") {
+            const title = await screen.findByRole(fieldId, {
+              name: "Project Details",
+            });
+            expect(title).toBeInTheDocument();
+          }
+
+          if (findBy === "label") {
+            const field = (await screen.findByLabelText(
+              RegExp(`^${getReadableInputName(fieldId)}`, "i")
+            )) as FormElement;
+
+            expect(field).toBeInTheDocument();
+            compareValue<IProjectDocument>(field.value, project!, fieldId);
+          }
+        });
+      }
+    );
+
+    /**
+     * Test on change functionality
+     */
+    testEach(
+      fieldsToTest,
+      "when on edit mode, can change $fieldId field's value",
+      async ({ fieldId, newFieldValue }) => {
+        user.setup();
+
+        act(() => renderWithRouter(<ProjectDetails />, auth));
+
+        const [field, fieldLabel] = await getFieldByLabel(fieldId);
+
+        const fieldEditButton = screen.queryByRole("button", {
+          name: RegExp(`^edit ${fieldLabel}`, "i"),
+        });
+
+        expect(field).toHaveAttribute("disabled");
+        expect(fieldEditButton).toBeInTheDocument();
+
+        await user.click(fieldEditButton!);
+        expect(
+          screen.queryByRole("button", {
+            name: RegExp(`^edit ${fieldLabel}`, "i"),
+          })
+        ).not.toBeInTheDocument();
+
+        const fieldSaveButton = screen.queryByRole("button", {
+          name: RegExp(`^save ${fieldLabel}`, "i"),
+        });
+
+        expect(fieldSaveButton).toBeInTheDocument();
+
+        await act(() => updateFieldAndCheckValue(field, newFieldValue));
+      }
+    );
+
+    /**
+     * Test save changes functionality
+     *
+     * TODO: Fix failing tests for the select fields:
+     * - newService
+     * - newTeamMember
+     */
+    testEach(
+      fieldsToTestNoSelects,
+      "when saving changes on $fieldId, the correct data is sent to the server",
+      async ({ fieldId, newFieldValue }) => {
+        user.setup();
+
+        act(() =>
+          renderWithRouterFromRoute(
+            componentWithAuthContext(<ProjectDetails />, auth),
+            [`/projects/${project?._id}`],
+            "/projects/:projectId"
+          )
+        );
+
+        const [field, fieldLabel] = await getFieldByLabel(fieldId);
+
+        const fieldEditButton = screen.queryByRole("button", {
+          name: RegExp(`^edit ${fieldLabel}`, "i"),
+        });
+
+        expect(field).toHaveAttribute("disabled");
+        expect(fieldEditButton).toBeInTheDocument();
+
+        await user.click(fieldEditButton!);
+
+        expect(
+          screen.queryByRole("button", {
+            name: RegExp(`^edit ${fieldLabel}`, "i"),
+          })
+        ).not.toBeInTheDocument();
+
+        const fieldSaveButton = screen.queryByRole("button", {
+          name: RegExp(`^save ${fieldLabel}`, "i"),
+        });
+
+        expect(fieldSaveButton).toBeInTheDocument();
+
+        await act(() => updateFieldAndCheckValue(field, newFieldValue));
+
+        await user.click(fieldSaveButton!);
+
+        const body = {
+          [fieldId]: newFieldValue,
+        };
+
+        await waitFor(async () => {
+          expect(requestUpdateResourceMock).toHaveBeenCalled();
+        });
+
+        expect(requestUpdateResourceMock).toHaveBeenCalledWith({
+          url: `${PROJECTS_BASE_API_URL}/${project?._id}`,
+          body,
+        });
+      }
+    );
+
+    /**
+     * Test cancel changes functionality
+     *
+     * TODO: Fix failing tests for the select fields:
+     * - newService
+     * - newTeamMember
+     */
+    testEach(
+      fieldsToTestNoSelects,
+      "when on edit mode, can cancel changes on $fieldId field's value",
+      async ({ fieldId, newFieldValue }) => {
+        user.setup();
+
+        act(() => renderWithRouter(<ProjectDetails />, auth));
+
+        const [field, fieldLabel] = await getFieldByLabel(fieldId);
+
+        const fieldEditButton = screen.queryByRole("button", {
+          name: RegExp(`^edit ${fieldLabel}`, "i"),
+        });
+        const fieldCancelButton = await screen.findByRole("button", {
+          name: RegExp(`^cancel changes to ${fieldLabel}`, "i"),
+        });
+
+        expect(fieldCancelButton).toBeInTheDocument();
+
+        await act(async () => {
+          await user.click(fieldEditButton!);
+        });
+
+        await act(() => updateFieldAndCheckValue(field, newFieldValue));
+
+        await act(async () => {
+          await user.click(fieldCancelButton!);
+        });
+
+        await waitFor(() => {
+          compareValue<IProjectDocument>(
+            field.value,
+            project!,
+            fieldId as keyof IProjectDocument
+          );
+        });
+      }
+    );
+  });
+});
--- frontend/src/pages/resources/projects/ProjectDetails/index.tsx
+++ frontend/src/pages/resources/projects/ProjectDetails/index.tsx
@@ -1,253 +1,128 @@
 import { ObjectId } from "mongoose";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { Link, useParams } from "react-router-dom";
 import {
   IProjectDocument,
   UserRole,
 } from "../../../../../../shared/interfaces";
-import Button from "../../../../components/Button";
-import FormControlWithActions from "../../../../components/FormControlWithActions";
-import Heading from "../../../../components/Heading";
 import { TextInput, UrlInput } from "../../../../components/Input";
 import SelectWithFetch from "../../../../components/Select/SelectWithFetch";
 import TextArea from "../../../../components/TextArea";
+import UpdatableDetailsForm from "../../../../components/UpdatableDetailsForm";
 import { useAuthContext } from "../../../../context/AuthProvider";
-import useFetch from "../../../../hooks/useFetch";
-import useValidation from "../../../../hooks/useValidation";
+import { FormElement } from "../../../../interfaces";
 import {
   PROJECTS_BASE_API_URL,
   SERVICES_BASE_API_URL,
   USERS_BASE_API_URL,
-  getDeleteOptions,
-  getUpdateOptions,
 } from "../../../../routes";
 import { getServiceDataOptions, getUserDataOptions } from "../../../../utils";
 
-type PartialDocument = Partial<IProjectDocument> & {
+type ProjectFormData = Partial<IProjectDocument> & {
   newService?: ObjectId | Record<string, unknown>;
   newTeamMember?: ObjectId | Record<string, unknown>;
 };
+
+const fields = [
+  {
+    Component: TextInput,
+    label: "Name:",
+    id: "name",
+    required: true,
+  },
+  {
+    Component: UrlInput,
+    label: "URL:",
+    id: "url",
+  },
+  {
+    Component: TextArea,
+    label: "Description:",
+    id: "description",
+  },
+  {
+    Component: SelectWithFetch,
+    label: "Add service:",
+    id: "newService",
+    fieldProps: {
+      url: SERVICES_BASE_API_URL,
+      getFormattedOptions: getServiceDataOptions,
+      showList: true,
+      pathToValue: "services",
+    },
+  },
+  {
+    Component: SelectWithFetch,
+    label: "Add team member:",
+    id: "newTeamMember",
+    ensureAdmin: true,
+    fieldProps: {
+      url: USERS_BASE_API_URL,
+      getFormattedOptions: getUserDataOptions,
+      showList: true,
+      pathToValue: "team",
+    },
+  },
+];
 
 const ProjectDetails = () => {
   const { user } = useAuthContext();
   const isAdmin = user?.role === UserRole.ADMIN;
   const params = useParams();
   const projectId = params.projectId;
-  const {
-    data: project,
-    loading,
-    error,
-    sendRequest,
-  } = useFetch<IProjectDocument | null>();
 
-  const [initialFormData, setInitialFormData] =
-    useState<PartialDocument | null>(null);
-  const [formData, setFormData] = useState<PartialDocument | null>(null);
-  const [changedFormData, setChangedFormData] = useState<PartialDocument>({});
-  const [errors, setErrors] = useState<{ [key: string]: string[] } | null>(
-    null
-  );
-  const { validateField } = useValidation();
-
-  const getProjectInfo = useCallback(() => {
-    sendRequest({ url: `${PROJECTS_BASE_API_URL}/${projectId}` });
-  }, [projectId, sendRequest]);
-
-  const requestDelete = () => {
-    sendRequest({
-      url: `${PROJECTS_BASE_API_URL}/${projectId}`,
-      options: getDeleteOptions(),
-    });
+  const formShape: ProjectFormData = {
+    name: "",
+    description: "",
+    url: "",
+    newService: "" as unknown as ObjectId,
+    newTeamMember: "" as unknown as ObjectId,
+    services: [],
   };
 
-  const handleChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => {
-    const target = e.target;
+  const handleChange = useCallback(
+    (target: FormElement, updatedFormData: ProjectFormData) => {
+      const newFormData = { ...updatedFormData };
 
-    setFormData({
-      ...formData!,
-      [target.name]:
-        target.type === "checkbox"
-          ? (target as HTMLInputElement).checked
-          : target.value !== "" && target.value
-          ? target.value
-          : null,
-    });
-
-    validateField({
-      target,
-      setErrors,
-    });
-  };
-
-  const handleCancel = (
-    target: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement,
-    initialValue:
-      | string
-      | number
-      | boolean
-      | readonly string[]
-      | ObjectId
-      | Record<string, unknown>
-  ) => {
-    setFormData({
-      ...formData!,
-      [target.id]: initialValue,
-    });
-  };
-
-  const handleSave = async () => {
-    const options = getUpdateOptions(changedFormData);
-    await sendRequest({
-      url: `${PROJECTS_BASE_API_URL}/${projectId}`,
-      options,
-    });
-    getProjectInfo();
-  };
-
-  const handleDelete = () => {
-    requestDelete();
-  };
-
-  useEffect(() => getProjectInfo(), [getProjectInfo]);
-
-  useEffect(() => {
-    if (project && !loading) {
-      const formDataShape: PartialDocument = {
-        name: project.name,
-        description: project.description,
-        url: project.url,
-        newService: undefined,
-        newTeamMember: undefined,
-        services: project.services,
-      };
-      setInitialFormData(formDataShape);
-      setFormData(formDataShape);
-    }
-  }, [loading, project]);
-
-  useEffect(() => {
-    const changes: PartialDocument = {};
-
-    for (const key in formData) {
-      if (
-        initialFormData &&
-        formData[key as keyof IProjectDocument] !==
-          initialFormData[key as keyof IProjectDocument]
-      ) {
-        if (key === "newService" && formData[key] !== undefined) {
-          changes.services = [
-            ...(project?.services || []),
-            formData.newService as ObjectId | Record<string, unknown>,
-          ];
-        } else if (key === "newTeamMember" && formData[key] !== undefined) {
-          changes.team = [
-            ...(project?.team || []),
-            formData.newTeamMember as ObjectId | Record<string, unknown>,
-          ];
-        } else {
-          changes[key as keyof IProjectDocument] =
-            formData[key as keyof IProjectDocument];
-        }
+      if (target.name === "newTeamMember") {
+        newFormData[target.name as keyof IProjectDocument] = target.value;
+        newFormData.team = [
+          ...(newFormData?.team || []),
+          target.value as unknown as ObjectId | Record<string, unknown>,
+        ];
+      } else if (target.name === "newService") {
+        newFormData[target.name as keyof IProjectDocument] = target.value;
+        newFormData.services = [
+          ...(newFormData?.services || []),
+          target.value as unknown as ObjectId | Record<string, unknown>,
+        ];
+      } else {
+        newFormData[target.name as keyof IProjectDocument] =
+          target.type === "checkbox"
+            ? (target as HTMLInputElement).checked
+            : target.value !== "" && target.value
+            ? target.value
+            : null;
       }
-    }
-    setChangedFormData(changes);
-  }, [formData, initialFormData, project]);
 
-  if (loading) {
-    return <Heading text="Loading..." level={1} role="status" />;
-  }
-
-  if (error) {
-    return <Heading text={error.message} level={1} role="status" />;
-  }
-
-  if (!project) {
-    return <Heading text="Project not found" level={1} role="status" />;
-  }
+      return newFormData;
+    },
+    []
+  );
 
   return (
-    project &&
-    !loading &&
-    formData && (
-      <div className="project-details flex flex-col align-stretch">
-        <div className="project-details__actions self-end flex gap-4">
-          <Button onClick={handleDelete}>Delete</Button>
-        </div>
-        <Heading text="Project details" level={1} />
-        <FormControlWithActions
-          label="Name:"
-          id="name"
-          onChange={handleChange}
-          onCancel={handleCancel}
-          onSave={handleSave}
-          value={formData?.name}
-          required
-          errors={errors}
-          setErrors={setErrors}
-          component={TextInput}
-        />
-        <FormControlWithActions
-          label="URL:"
-          id="url"
-          onChange={handleChange}
-          onCancel={handleCancel}
-          onSave={handleSave}
-          value={formData?.url}
-          errors={errors}
-          setErrors={setErrors}
-          component={UrlInput}
-        />
-        <FormControlWithActions
-          label="Description:"
-          id="description"
-          onChange={handleChange}
-          onCancel={handleCancel}
-          onSave={handleSave}
-          value={formData?.description}
-          errors={errors}
-          setErrors={setErrors}
-          component={TextArea}
-        />
-        <FormControlWithActions
-          label="Add service:"
-          id="newService"
-          onChange={handleChange}
-          onCancel={handleCancel}
-          onSave={handleSave}
-          value={formData?.newService}
-          url={SERVICES_BASE_API_URL}
-          getFormattedOptions={getServiceDataOptions}
-          errors={errors}
-          setErrors={setErrors}
-          component={SelectWithFetch}
-          showList={true}
-          currentList={formData.services}
-        />
-        {isAdmin ? (
-          <FormControlWithActions
-            label="Add team member:"
-            id="newTeamMember"
-            onChange={handleChange}
-            onCancel={handleCancel}
-            onSave={handleSave}
-            value={formData?.newTeamMember}
-            url={USERS_BASE_API_URL}
-            getFormattedOptions={getUserDataOptions}
-            errors={errors}
-            setErrors={setErrors}
-            component={SelectWithFetch}
-            showList={true}
-            currentList={formData.team}
-          />
-        ) : (
-          <Link to="/dashboard/users/create">Add team member</Link>
-        )}
-      </div>
-    )
+    <>
+      <UpdatableDetailsForm
+        resourceUrl={PROJECTS_BASE_API_URL}
+        resourceId={projectId as string}
+        resourceName={"project"}
+        onChange={handleChange}
+        formShape={formShape}
+        fields={fields}
+        isAdmin={isAdmin}
+      />
+      {!isAdmin && <Link to="/dashboard/users/create">Add team member</Link>}
+    </>
   );
 };
 
