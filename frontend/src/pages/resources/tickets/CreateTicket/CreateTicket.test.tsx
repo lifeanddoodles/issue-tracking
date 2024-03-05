c64421019d2ddd22d40d609842af4845307d2f7f
“Sandra <sandravargasgro@gmail.com>
2024-03-05 00:15:10 -0800

    Refactor CreateTicket and add unit tests

---
+++ frontend/src/pages/resources/tickets/CreateTicket/CreateTicket.test.tsx
@@ -0,0 +1,304 @@
+import { act, screen, waitFor } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import CreateTicket from ".";
+import { newFakeTicket } from "../../../../__mocks__";
+import useForm from "../../../../hooks/useForm";
+import { FormElement, TestCaseProps } from "../../../../interfaces";
+import {
+  IAuthContext,
+  adminAuth,
+  getFieldByLabel,
+  renderWithRouter,
+  testEach,
+  updateFieldAndCheckValue,
+} from "../../../../tests/utils";
+import { getReadableInputName } from "../../../../utils";
+
+vi.mock("../../../../hooks/useForm");
+
+const fieldsToTest = [
+  { fieldId: "title", newFieldValue: newFakeTicket.title, customLabel: null },
+  {
+    fieldId: "description",
+    newFieldValue: newFakeTicket.description,
+    customLabel: null,
+  },
+  { fieldId: "status", newFieldValue: newFakeTicket.status, customLabel: null },
+  {
+    fieldId: "assignToTeam",
+    newFieldValue: newFakeTicket.assignToTeam,
+    customLabel: null,
+  },
+  {
+    fieldId: "assignee",
+    newFieldValue: newFakeTicket.assignee,
+    customLabel: null,
+  },
+  {
+    fieldId: "reporter",
+    newFieldValue: newFakeTicket.reporter,
+    customLabel: null,
+  },
+  {
+    fieldId: "priority",
+    newFieldValue: newFakeTicket.priority,
+    customLabel: null,
+  },
+  {
+    fieldId: "ticketType",
+    newFieldValue: newFakeTicket.ticketType,
+    customLabel: null,
+  },
+  {
+    fieldId: "estimatedTime",
+    newFieldValue: newFakeTicket.estimatedTime,
+    customLabel: null,
+  },
+  {
+    fieldId: "deadline",
+    newFieldValue: newFakeTicket.deadline,
+    customLabel: null,
+  },
+  {
+    fieldId: "isSubtask",
+    newFieldValue: newFakeTicket.isSubtask,
+    customLabel: null,
+  },
+  {
+    fieldId: "parentTask",
+    newFieldValue: newFakeTicket.parentTask,
+    customLabel: null,
+  },
+];
+
+const formDataShape = {
+  title: "",
+  description: "",
+  externalReporter: "",
+  assignToTeam: "",
+  assignee: "",
+  reporter: "",
+  status: "",
+  priority: "",
+  ticketType: "",
+  estimatedTime: "",
+  deadline: "",
+  isSubtask: false,
+  parentTask: "",
+};
+
+const filledFormData = {
+  title: newFakeTicket.title,
+  description: newFakeTicket.description,
+  assignToTeam: newFakeTicket.assignToTeam,
+  assignee: newFakeTicket.assignee,
+  reporter: newFakeTicket.reporter,
+  status: newFakeTicket.status,
+  priority: newFakeTicket.priority,
+  ticketType: newFakeTicket.ticketType,
+  estimatedTime: newFakeTicket.estimatedTime,
+  deadline: newFakeTicket.deadline,
+  isSubtask: newFakeTicket.isSubtask,
+  parentTask: newFakeTicket.parentTask,
+};
+
+describe("CreateTicket", () => {
+  let auth: IAuthContext;
+  const onSubmitSpy = vi.fn(() => {
+    return Promise.resolve();
+  });
+
+  beforeAll(() => {
+    vi.clearAllMocks();
+  });
+
+  describe("on initial render", async () => {
+    beforeEach(async () => {
+      vi.mocked(useForm)
+        .mockClear()
+        .mockImplementation(() => ({
+          formData: formDataShape,
+          setFormData: vi.fn(),
+          errors: null,
+          setErrors: vi.fn(),
+          onSubmit: onSubmitSpy,
+          setObjectShape: vi.fn(),
+          initialFormData: vi.fn(),
+          setInitialFormData: vi.fn(),
+          changedFormData: vi.fn(),
+          setChangedFormData: vi.fn(),
+          data: null,
+          loading: false,
+          error: null,
+          requestGetResource: vi.fn(),
+          requestPostResource: vi.fn(),
+          requestUpdateResource: vi.fn(),
+          requestDeleteResource: vi.fn(),
+        }));
+
+      auth = {
+        ...adminAuth,
+      };
+    });
+
+    test.each`
+      fieldId            | findBy    | customLabel
+      ${"heading"}       | ${"role"} | ${"NULL"}
+      ${"title"}         | ${"text"} | ${"NULL"}
+      ${"description"}   | ${"text"} | ${"NULL"}
+      ${"status"}        | ${"text"} | ${"NULL"}
+      ${"assignToTeam"}  | ${"text"} | ${"NULL"}
+      ${"assignee"}      | ${"text"} | ${"NULL"}
+      ${"reporter"}      | ${"text"} | ${"NULL"}
+      ${"priority"}      | ${"text"} | ${"NULL"}
+      ${"ticketType"}    | ${"text"} | ${"NULL"}
+      ${"estimatedTime"} | ${"text"} | ${"NULL"}
+      ${"deadline"}      | ${"text"} | ${"NULL"}
+      ${"isSubtask"}     | ${"text"} | ${"NULL"}
+      ${"parentTask"}    | ${"text"} | ${"NULL"}
+    `(
+      "renders $fieldId with the correct value",
+      async ({ fieldId, findBy, customLabel }) => {
+        act(() => renderWithRouter(<CreateTicket />, auth));
+
+        await waitFor(async () => {
+          if (findBy === "role") {
+            const title = await screen.findByRole(fieldId, {
+              name: "Create ticket",
+            });
+            expect(title).toBeInTheDocument();
+          }
+
+          if (findBy === "text") {
+            const label =
+              customLabel !== "NULL"
+                ? customLabel
+                : getReadableInputName(fieldId);
+            const field = (await screen.findByLabelText(
+              RegExp(`^${label}`, "i")
+            )) as FormElement;
+
+            expect(field).toBeInTheDocument();
+            expect(field.value).toBe("");
+          }
+        });
+      }
+    );
+  });
+
+  /**
+   * Test on change functionality
+   */
+  describe("when filling the form", async () => {
+    beforeEach(async () => {
+      vi.mocked(useForm)
+        .mockClear()
+        .mockImplementation(() => ({
+          formData: formDataShape,
+          setFormData: vi.fn(),
+          errors: null,
+          setErrors: vi.fn(),
+          onSubmit: onSubmitSpy,
+          setObjectShape: vi.fn(),
+          initialFormData: vi.fn(),
+          setInitialFormData: vi.fn(),
+          changedFormData: vi.fn(),
+          setChangedFormData: vi.fn(),
+          data: null,
+          loading: false,
+          error: null,
+          requestGetResource: vi.fn(),
+          requestPostResource: vi.fn(),
+          requestUpdateResource: vi.fn(),
+          requestDeleteResource: vi.fn(),
+        }));
+
+      auth = {
+        ...adminAuth,
+      };
+    });
+
+    testEach(
+      fieldsToTest as TestCaseProps[],
+      "can change $fieldId field's value",
+      async ({ fieldId, newFieldValue, customLabel }) => {
+        user.setup();
+
+        act(() => renderWithRouter(<CreateTicket />, auth));
+
+        const [field] = await getFieldByLabel(customLabel ?? fieldId);
+
+        await act(() => updateFieldAndCheckValue(field, newFieldValue));
+      }
+    );
+  });
+
+  /**
+   * Test submit functionality
+   */
+  describe.only("when submitting", async () => {
+    beforeEach(async () => {
+      vi.mocked(useForm)
+        .mockClear()
+        .mockImplementation(() => ({
+          formData: filledFormData,
+          setFormData: vi.fn(),
+          errors: null,
+          setErrors: vi.fn(),
+          onSubmit: onSubmitSpy,
+          setObjectShape: vi.fn(),
+          initialFormData: vi.fn(),
+          setInitialFormData: vi.fn(),
+          changedFormData: vi.fn(),
+          setChangedFormData: vi.fn(),
+          data: null,
+          loading: false,
+          error: null,
+          requestGetResource: vi.fn(),
+          requestPostResource: vi.fn(),
+          requestUpdateResource: vi.fn(),
+          requestDeleteResource: vi.fn(),
+        }));
+
+      auth = {
+        ...adminAuth,
+      };
+    });
+
+    test("sends the correct values", async () => {
+      user.setup();
+
+      act(() => renderWithRouter(<CreateTicket />, auth));
+
+      for (const { fieldId, newFieldValue } of fieldsToTest) {
+        const [field] = await getFieldByLabel(fieldId);
+
+        expect(field).toBeInTheDocument();
+
+        await act(() =>
+          updateFieldAndCheckValue(
+            field,
+            newFieldValue as string | number | boolean
+          )
+        );
+      }
+
+      const submitButton = screen.getByRole("button", {
+        name: "Submit",
+      });
+
+      expect(submitButton).toBeInTheDocument();
+
+      await user.click(submitButton);
+
+      const body = {
+        ...filledFormData,
+      };
+
+      await waitFor(async () => {
+        expect(onSubmitSpy).toHaveBeenCalled();
+        expect(onSubmitSpy).toHaveBeenCalledWith("POST", body);
+      });
+    });
+  });
+});
--- frontend/src/pages/resources/tickets/CreateTicket/index.tsx
+++ frontend/src/pages/resources/tickets/CreateTicket/index.tsx
@@ -1,9 +1,11 @@
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import {
+  DepartmentTeam,
   ITicket,
   ITicketBase,
   ITicketDocument,
+  IUserDocument,
   Priority,
   Status,
   TicketType,
@@ -12,7 +14,7 @@ import {
 import Button from "../../../../components/Button";
 import Form from "../../../../components/Form";
 import Heading from "../../../../components/Heading";
-import Input, { TextInput } from "../../../../components/Input";
+import { TextInput } from "../../../../components/Input";
 import Select from "../../../../components/Select";
 import SelectWithFetch from "../../../../components/Select/SelectWithFetch";
 import TextArea from "../../../../components/TextArea";
@@ -20,25 +22,156 @@ import Toggle from "../../../../components/Toggle";
 import { useAuthContext } from "../../../../context/AuthProvider";
 import useForm from "../../../../hooks/useForm";
 import useValidation from "../../../../hooks/useValidation";
-import {
-  TICKETS_BASE_API_URL,
-  USERS_BASE_API_URL,
-  getPostOptions,
-} from "../../../../routes";
+import { TICKETS_BASE_API_URL, USERS_BASE_API_URL } from "../../../../routes";
 import {
   getAssignableDepartmentTeamOptions,
+  getNonClientDataOptions,
   getPriorityOptions,
   getStatusOptions,
   getTicketDataOptions,
   getTicketTypeOptions,
   getUserDataOptions,
+  renderFields,
 } from "../../../../utils";
+
+type CreateTicketFormData = Partial<ITicketDocument>;
+
+const getDepartmentQuery = (assignToTeam: DepartmentTeam) => {
+  return assignToTeam && assignToTeam ? `?department=${assignToTeam}` : "";
+};
+
+const isParentTaskDisabled = (subTaskExists: boolean = false) => {
+  return !subTaskExists;
+};
+
+const isFieldDisabled = (id: string, getArgs?: (id: string) => unknown) => {
+  let arg;
+  switch (id) {
+    case "parentTask":
+      arg = getArgs?.(id);
+      return isParentTaskDisabled(arg as boolean);
+    default:
+      return;
+  }
+};
+
+const fields = [
+  {
+    Component: TextInput,
+    label: "Title:",
+    id: "title",
+    required: true,
+  },
+  {
+    Component: TextArea,
+    label: "Description:",
+    id: "description",
+    required: true,
+  },
+  {
+    Component: TextInput,
+    label: "Reporter:",
+    id: "externalReporter",
+    required: true,
+    permissions: { VIEW: [UserRole.CLIENT] },
+  },
+  {
+    Component: Select,
+    label: "Assign to team:",
+    id: "assignToTeam",
+    required: true,
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+    fieldProps: {
+      options: getAssignableDepartmentTeamOptions,
+    },
+  },
+  {
+    Component: SelectWithFetch,
+    label: "Assignee:",
+    id: "assignee",
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+    fieldProps: {
+      url: USERS_BASE_API_URL,
+      getFormattedOptions: getUserDataOptions,
+      query: true,
+    },
+  },
+  {
+    Component: SelectWithFetch,
+    label: "Reporter:",
+    id: "reporter",
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+    fieldProps: {
+      url: USERS_BASE_API_URL,
+      getFormattedOptions: getNonClientDataOptions,
+    },
+  },
+  {
+    Component: Select,
+    label: "Status:",
+    id: "status",
+    required: true,
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+    fieldProps: {
+      options: getStatusOptions,
+    },
+  },
+  {
+    Component: Select,
+    label: "Priority:",
+    id: "priority",
+    required: true,
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+    fieldProps: {
+      options: getPriorityOptions,
+    },
+  },
+  {
+    Component: Select,
+    label: "Type:",
+    id: "ticketType",
+    required: true,
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+    fieldProps: {
+      options: getTicketTypeOptions,
+    },
+  },
+  {
+    Component: TextInput,
+    label: "Estimated time:",
+    id: "estimatedTime",
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+  },
+  {
+    Component: TextInput,
+    label: "Deadline:",
+    id: "deadline",
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+  },
+  {
+    Component: Toggle,
+    label: "Is subtask:",
+    id: "isSubtask",
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+  },
+  {
+    Component: SelectWithFetch,
+    label: "Parent task:",
+    id: "parentTask",
+    disabled: isFieldDisabled,
+    permissions: { VIEW: [UserRole.ADMIN, UserRole.DEVELOPER, UserRole.STAFF] },
+    fieldProps: {
+      url: TICKETS_BASE_API_URL,
+      getFormattedOptions: getTicketDataOptions,
+    },
+  },
+];
 
 const CreateTicket = () => {
   const navigate = useNavigate();
   const { user } = useAuthContext();
-  const role = user?.role;
-  const isClient = role === UserRole.CLIENT;
+  const userRole = user?.role as UserRole;
+  const isClient = userRole === UserRole.CLIENT;
   const formDataShape = isClient
     ? {
         title: "",
@@ -51,7 +184,7 @@ const CreateTicket = () => {
         description: "",
         attachments: [],
         assignee: "",
-        reporter: user!._id,
+        reporter: (user as Partial<IUserDocument>)._id,
         status: Status.OPEN,
         priority: Priority.MEDIUM,
         assignToTeam: "",
@@ -62,7 +195,7 @@ const CreateTicket = () => {
         parentTask: "",
       };
   const { formData, setFormData, errors, setErrors, onSubmit, data } = useForm<
-    (ITicketBase | ITicket) & { teamMember: string },
+    CreateTicketFormData,
     ITicketDocument
   >({
     formShape: formDataShape as Partial<ITicketBase | ITicket>,
@@ -72,177 +205,109 @@ const CreateTicket = () => {
     },
   });
   const { validateField } = useValidation();
-  const assignToTeam = (formData as Partial<ITicket>)?.assignToTeam || "";
+
+  const assignToTeam = useMemo(
+    () => (formData as CreateTicketFormData)?.assignToTeam || "",
+    [formData]
+  );
   const departmentQuery = useMemo(() => {
-    return assignToTeam && assignToTeam ? `?department=${assignToTeam}` : "";
+    return assignToTeam && getDepartmentQuery(assignToTeam);
   }, [assignToTeam]);
 
-  const handleChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => {
-    const target = e.target;
+  const hasSubtask = useMemo(() => {
+    return (formData as Partial<ITicket>)?.isSubtask || false;
+  }, [formData]);
 
-    setFormData({
-      ...formData,
-      [target.name]: target.value,
-    });
+  const getIsFieldDisabledArgs = useCallback(
+    (id: string) => {
+      switch (id) {
+        case "parentTask":
+          return hasSubtask;
+        default:
+          return;
+      }
+    },
+    [hasSubtask]
+  );
 
-    validateField({
-      target,
-      setErrors,
-    });
-  };
+  const handleChange = useCallback(
+    (
+      e: React.ChangeEvent<
+        HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+      >
+    ) => {
+      const target = e.target;
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+      setFormData({
+        ...formData,
+        [target.name]:
+          target.type === "checkbox"
+            ? (target as HTMLInputElement).checked
+            : target.value,
+      });
 
-    const options = getPostOptions(formData);
-    onSubmit(options);
-  };
+      validateField({
+        target,
+        setErrors,
+      });
+    },
+    [formData, setErrors, setFormData, validateField]
+  );
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      onSubmit("POST", formData);
+    },
+    [formData, onSubmit]
+  );
+
+  const fieldsWithFormProps = useCallback(
+    (formShape: CreateTicketFormData) =>
+      fields.map((field) => {
+        const isDisabled =
+          typeof field.disabled === "function"
+            ? field.disabled(field.id, getIsFieldDisabledArgs)
+            : field.disabled;
+
+        return {
+          ...field,
+          name: field.id,
+          [typeof formShape![field.id as keyof typeof formShape] === "boolean"
+            ? "checked"
+            : "value"]: formShape![field.id as keyof typeof formShape] as
+            | string
+            | boolean,
+          onChange: handleChange,
+          disabled: isDisabled,
+          errors,
+          setErrors,
+          ...(field?.fieldProps?.query ? { query: departmentQuery } : {}),
+        };
+      }),
+    [departmentQuery, errors, getIsFieldDisabledArgs, handleChange, setErrors]
+  );
+
+  const getRenderedChildren = useCallback(
+    (formShape: CreateTicketFormData) =>
+      renderFields(fieldsWithFormProps(formShape), formShape, userRole),
+    [fieldsWithFormProps, userRole]
+  );
+
+  const renderedChildren = useMemo(
+    () => formData !== null && getRenderedChildren(formData),
+    [formData, getRenderedChildren]
+  );
 
   return (
-    <Form onSubmit={handleSubmit} className="ml-0">
-      <Heading text="Create ticket" level={1} />
-      <Input
-        label="Title:"
-        id="title"
-        onChange={handleChange}
-        value={formData.title}
-        required
-        errors={errors}
-        setErrors={setErrors}
-      />
-      <TextArea
-        label="Description:"
-        id="description"
-        onChange={handleChange}
-        value={formData.description}
-        required
-        errors={errors}
-        setErrors={setErrors}
-      />
-      <Select
-        label={isClient ? "Reporter:" : "External reporter:"}
-        id="externalReporter"
-        value={formData.externalReporter?.toString()}
-        options={isClient ? getUserDataOptions([user!]) : []}
-        onChange={handleChange}
-        required
-        errors={errors}
-        setErrors={setErrors}
-        disabled
-      />
-      {!isClient && (
-        <>
-          <Select
-            label="Assign to team:"
-            id="assignToTeam"
-            value={(formData as Partial<ITicket>)?.assignToTeam}
-            options={getAssignableDepartmentTeamOptions()}
-            onChange={handleChange}
-            required
-            errors={errors}
-            setErrors={setErrors}
-          />
-          <SelectWithFetch
-            label="Assignee:"
-            id="assignee"
-            value={(formData as Partial<ITicket>)?.assignee?.toString() || ""}
-            onChange={handleChange}
-            disabled={isClient}
-            errors={errors}
-            url={USERS_BASE_API_URL}
-            query={departmentQuery}
-            getFormattedOptions={getUserDataOptions}
-          />
-          <SelectWithFetch
-            label="Reporter:"
-            id="reporter"
-            value={(formData as Partial<ITicket>).reporter?.toString() || ""}
-            onChange={handleChange}
-            disabled={isClient}
-            errors={errors}
-            url={USERS_BASE_API_URL}
-            query={departmentQuery}
-            getFormattedOptions={getUserDataOptions}
-          />
-          <Select
-            label="Status:"
-            id="status"
-            value={(formData as Partial<ITicket>)?.status}
-            options={getStatusOptions()}
-            onChange={handleChange}
-            required
-            errors={errors}
-            setErrors={setErrors}
-          />
-          <Select
-            label="Priority:"
-            id="priority"
-            value={(formData as Partial<ITicket>)?.priority}
-            options={getPriorityOptions()}
-            onChange={handleChange}
-            required
-            errors={errors}
-            setErrors={setErrors}
-          />
-          <Select
-            label="Type:"
-            id="ticketType"
-            value={(formData as Partial<ITicket>)?.ticketType}
-            options={getTicketTypeOptions()}
-            onChange={handleChange}
-            required
-            errors={errors}
-            setErrors={setErrors}
-          />
-          <TextInput
-            label="Estimated time:"
-            id="estimatedTime"
-            onChange={handleChange}
-            value={(formData as Partial<ITicket>)?.estimatedTime ?? ""}
-            required
-            errors={errors}
-            setErrors={setErrors}
-          />
-          <TextInput
-            label="Deadline:"
-            id="deadline"
-            onChange={handleChange}
-            value={(formData as Partial<ITicket>)?.deadline as string}
-            required
-            errors={errors}
-            setErrors={setErrors}
-          />
-          <Toggle
-            label="Is subtask:"
-            id="isSubtask"
-            onChange={handleChange}
-            checked={(formData as Partial<ITicket>)?.isSubtask || false}
-            errors={errors}
-            setErrors={setErrors}
-          />
-          <SelectWithFetch
-            label="Parent task:"
-            id="parentTask"
-            value={
-              !(formData as Partial<ITicket>)?.isSubtask ||
-              !(formData as Partial<ITicket>)?.parentTask
-                ? ""
-                : (formData as Partial<ITicket>)?.parentTask?.toString() || ""
-            }
-            onChange={handleChange}
-            disabled={isClient || !(formData as Partial<ITicket>)?.isSubtask}
-            errors={errors}
-            url={TICKETS_BASE_API_URL}
-            getFormattedOptions={getTicketDataOptions}
-          />
-        </>
-      )}
-      <Button type="submit">Submit</Button>
-    </Form>
+    formData && (
+      <Form onSubmit={handleSubmit} className="ml-0">
+        <Heading text="Create ticket" level={1} />
+        {renderedChildren}
+        <Button type="submit">Submit</Button>
+      </Form>
+    )
   );
 };
 
